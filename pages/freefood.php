c9b78549b42c8ce3a3ef4caa53c97820ef0236c4
Jiarui_Xu <sidewallme@gmail.com>
2014-10-21 15:55:22 -0500

    free food page

---
+++ pages/freefood.php
@@ -0,0 +1,32 @@
+<?php
+require_once("../php_files/db_const.php");
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+# check connection
+if ($mysqli->connect_errno) {
+    echo "<p>MySQL error no {$mysqli->connect_errno} : {$mysqli->connect_error}</p>";
+    exit();
+}
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$result = mysqli_query($mysqli,"SELECT * FROM events");
+
+echo "<table border='1'>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['id'] . "</td>";
+    echo "<td>" . $row['name'] . "</td>";
+    echo "</tr>";
+}
+
+echo "</table>";
+
+mysqli_close($mysqli);
+?>
